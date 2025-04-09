13b6cef0d211c676bcf1f0333e03406c7dc9fe7c
BossDaily <gboys2008@gmail.com>
2025-04-09 17:13:59 -0500

    Fixed font and button rendering issues

--- Geo/Form1.Designer.cs
+++ Geo/Form1.Designer.cs
@@ -65,10 +65,11 @@
             this.mainTabControl.Controls.Add(this.loginTab);
             this.mainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainTabControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mainTabControl.Location = new System.Drawing.Point(0, 1);
+            this.mainTabControl.Location = new System.Drawing.Point(0, 2);
+            this.mainTabControl.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.mainTabControl.Name = "mainTabControl";
             this.mainTabControl.SelectedIndex = 0;
-            this.mainTabControl.Size = new System.Drawing.Size(800, 448);
+            this.mainTabControl.Size = new System.Drawing.Size(1600, 861);
             this.mainTabControl.TabIndex = 0;
             // 
             // mainMenuTab
@@ -81,10 +82,11 @@
             this.mainMenuTab.Controls.Add(this.quitButton);
             this.mainMenuTab.Controls.Add(this.quickplayButton);
             this.mainMenuTab.Controls.Add(this.learnButton);
-            this.mainMenuTab.Location = new System.Drawing.Point(4, 25);
+            this.mainMenuTab.Location = new System.Drawing.Point(8, 44);
+            this.mainMenuTab.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.mainMenuTab.Name = "mainMenuTab";
-            this.mainMenuTab.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
-            this.mainMenuTab.Size = new System.Drawing.Size(792, 419);
+            this.mainMenuTab.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.mainMenuTab.Size = new System.Drawing.Size(1584, 809);
             this.mainMenuTab.TabIndex = 0;
             this.mainMenuTab.Text = "Main Menu";
             this.mainMenuTab.UseVisualStyleBackColor = true;
@@ -95,19 +97,19 @@
             this.quickPlayPanel.Controls.Add(this.quickPlay_PlayButton);
             this.quickPlayPanel.Controls.Add(this.quizModeRadioButton);
             this.quickPlayPanel.Controls.Add(this.mapModeRadioButton);
-            this.quickPlayPanel.Location = new System.Drawing.Point(419, 89);
-            this.quickPlayPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.quickPlayPanel.Location = new System.Drawing.Point(838, 171);
+            this.quickPlayPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.quickPlayPanel.Name = "quickPlayPanel";
-            this.quickPlayPanel.Size = new System.Drawing.Size(328, 227);
+            this.quickPlayPanel.Size = new System.Drawing.Size(656, 437);
             this.quickPlayPanel.TabIndex = 5;
             this.quickPlayPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.quickPlayPanel_Paint);
             // 
             // quickPlay_ReturnButton
             // 
-            this.quickPlay_ReturnButton.Location = new System.Drawing.Point(112, 2);
-            this.quickPlay_ReturnButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.quickPlay_ReturnButton.Location = new System.Drawing.Point(224, 4);
+            this.quickPlay_ReturnButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.quickPlay_ReturnButton.Name = "quickPlay_ReturnButton";
-            this.quickPlay_ReturnButton.Size = new System.Drawing.Size(71, 46);
+            this.quickPlay_ReturnButton.Size = new System.Drawing.Size(142, 88);
             this.quickPlay_ReturnButton.TabIndex = 3;
             this.quickPlay_ReturnButton.Text = "Return";
             this.quickPlay_ReturnButton.UseVisualStyleBackColor = true;
@@ -115,10 +117,10 @@
             // 
             // quickPlay_PlayButton
             // 
-            this.quickPlay_PlayButton.Location = new System.Drawing.Point(64, 75);
-            this.quickPlay_PlayButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.quickPlay_PlayButton.Location = new System.Drawing.Point(224, 185);
+            this.quickPlay_PlayButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.quickPlay_PlayButton.Name = "quickPlay_PlayButton";
-            this.quickPlay_PlayButton.Size = new System.Drawing.Size(38, 12);
+            this.quickPlay_PlayButton.Size = new System.Drawing.Size(116, 53);
             this.quickPlay_PlayButton.TabIndex = 2;
             this.quickPlay_PlayButton.Text = "Play";
             this.quickPlay_PlayButton.UseVisualStyleBackColor = true;
@@ -127,10 +129,10 @@
             // quizModeRadioButton
             // 
             this.quizModeRadioButton.AutoSize = true;
-            this.quizModeRadioButton.Location = new System.Drawing.Point(187, 44);
-            this.quizModeRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.quizModeRadioButton.Location = new System.Drawing.Point(374, 85);
+            this.quizModeRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.quizModeRadioButton.Name = "quizModeRadioButton";
-            this.quizModeRadioButton.Size = new System.Drawing.Size(98, 20);
+            this.quizModeRadioButton.Size = new System.Drawing.Size(177, 34);
             this.quizModeRadioButton.TabIndex = 1;
             this.quizModeRadioButton.TabStop = true;
             this.quizModeRadioButton.Text = "Quiz Mode";
@@ -140,10 +142,10 @@
             // mapModeRadioButton
             // 
             this.mapModeRadioButton.AutoSize = true;
-            this.mapModeRadioButton.Location = new System.Drawing.Point(20, 38);
-            this.mapModeRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.mapModeRadioButton.Location = new System.Drawing.Point(40, 73);
+            this.mapModeRadioButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.mapModeRadioButton.Name = "mapModeRadioButton";
-            this.mapModeRadioButton.Size = new System.Drawing.Size(98, 20);
+            this.mapModeRadioButton.Size = new System.Drawing.Size(173, 34);
             this.mapModeRadioButton.TabIndex = 0;
             this.mapModeRadioButton.TabStop = true;
             this.mapModeRadioButton.Text = "Map Mode";
@@ -152,31 +154,31 @@
             // 
             // learnPanel
             // 
-            this.learnPanel.Location = new System.Drawing.Point(3, 42);
-            this.learnPanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.learnPanel.Location = new System.Drawing.Point(6, 81);
+            this.learnPanel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.learnPanel.Name = "learnPanel";
-            this.learnPanel.Size = new System.Drawing.Size(176, 119);
+            this.learnPanel.Size = new System.Drawing.Size(352, 229);
             this.learnPanel.TabIndex = 4;
             this.learnPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.learnPanel_Paint);
             // 
             // gameNameLabel
             // 
             this.gameNameLabel.AutoSize = true;
-            this.gameNameLabel.Font = new System.Drawing.Font("Perpetua", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameNameLabel.Location = new System.Drawing.Point(142, 18);
-            this.gameNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.gameNameLabel.Font = new System.Drawing.Font("Segoe UI Variable Text", 28.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameNameLabel.Location = new System.Drawing.Point(397, 38);
+            this.gameNameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.gameNameLabel.Name = "gameNameLabel";
-            this.gameNameLabel.Size = new System.Drawing.Size(269, 42);
+            this.gameNameLabel.Size = new System.Drawing.Size(654, 100);
             this.gameNameLabel.TabIndex = 3;
             this.gameNameLabel.Text = "Geography Game";
             this.gameNameLabel.Click += new System.EventHandler(this.gameNameLabel_Click);
             // 
             // quitButton
             // 
-            this.quitButton.Location = new System.Drawing.Point(188, 137);
-            this.quitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.quitButton.Location = new System.Drawing.Point(477, 369);
+            this.quitButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.quitButton.Name = "quitButton";
-            this.quitButton.Size = new System.Drawing.Size(38, 12);
+            this.quitButton.Size = new System.Drawing.Size(106, 66);
             this.quitButton.TabIndex = 2;
             this.quitButton.Text = "Quit";
             this.quitButton.UseVisualStyleBackColor = true;
@@ -184,10 +186,10 @@
             // 
             // quickplayButton
             // 
-            this.quickplayButton.Location = new System.Drawing.Point(188, 89);
-            this.quickplayButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.quickplayButton.Location = new System.Drawing.Point(449, 219);
+            this.quickplayButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.quickplayButton.Name = "quickplayButton";
-            this.quickplayButton.Size = new System.Drawing.Size(38, 23);
+            this.quickplayButton.Size = new System.Drawing.Size(195, 71);
             this.quickplayButton.TabIndex = 1;
             this.quickplayButton.Text = "Quick Play";
             this.quickplayButton.UseVisualStyleBackColor = true;
@@ -195,10 +197,10 @@
             // 
             // learnButton
             // 
-            this.learnButton.Location = new System.Drawing.Point(188, 51);
-            this.learnButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.learnButton.Location = new System.Drawing.Point(376, 98);
+            this.learnButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.learnButton.Name = "learnButton";
-            this.learnButton.Size = new System.Drawing.Size(38, 12);
+            this.learnButton.Size = new System.Drawing.Size(76, 23);
             this.learnButton.TabIndex = 0;
             this.learnButton.Text = "Learn";
             this.learnButton.UseVisualStyleBackColor = true;
@@ -206,44 +208,44 @@
             // 
             // groupsTab
             // 
-            this.groupsTab.Location = new System.Drawing.Point(4, 25);
-            this.groupsTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.groupsTab.Location = new System.Drawing.Point(8, 44);
+            this.groupsTab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupsTab.Name = "groupsTab";
-            this.groupsTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.groupsTab.Size = new System.Drawing.Size(790, 419);
+            this.groupsTab.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupsTab.Size = new System.Drawing.Size(1584, 810);
             this.groupsTab.TabIndex = 1;
             this.groupsTab.Text = "Groups";
             this.groupsTab.UseVisualStyleBackColor = true;
             // 
             // leaderboardTab
             // 
-            this.leaderboardTab.Location = new System.Drawing.Point(4, 25);
-            this.leaderboardTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.leaderboardTab.Location = new System.Drawing.Point(8, 44);
+            this.leaderboardTab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.leaderboardTab.Name = "leaderboardTab";
-            this.leaderboardTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.leaderboardTab.Size = new System.Drawing.Size(790, 419);
+            this.leaderboardTab.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.leaderboardTab.Size = new System.Drawing.Size(1584, 810);
             this.leaderboardTab.TabIndex = 2;
             this.leaderboardTab.Text = "Leaderboard";
             this.leaderboardTab.UseVisualStyleBackColor = true;
             // 
             // settingsTab
             // 
-            this.settingsTab.Location = new System.Drawing.Point(4, 25);
-            this.settingsTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.settingsTab.Location = new System.Drawing.Point(8, 44);
+            this.settingsTab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.settingsTab.Name = "settingsTab";
-            this.settingsTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.settingsTab.Size = new System.Drawing.Size(790, 419);
+            this.settingsTab.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.settingsTab.Size = new System.Drawing.Size(1584, 810);
             this.settingsTab.TabIndex = 3;
             this.settingsTab.Text = "Settings";
             this.settingsTab.UseVisualStyleBackColor = true;
             // 
             // loginTab
             // 
-            this.loginTab.Location = new System.Drawing.Point(4, 25);
-            this.loginTab.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.loginTab.Location = new System.Drawing.Point(8, 44);
+            this.loginTab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loginTab.Name = "loginTab";
-            this.loginTab.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.loginTab.Size = new System.Drawing.Size(790, 419);
+            this.loginTab.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.loginTab.Size = new System.Drawing.Size(1584, 810);
             this.loginTab.TabIndex = 4;
             this.loginTab.Text = "Login";
             this.loginTab.UseVisualStyleBackColor = true;
@@ -299,16 +301,17 @@
             // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1600, 865);
             this.Controls.Add(this.mainTabControl);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "mainForm";
-            this.Padding = new System.Windows.Forms.Padding(0, 1, 0, 1);
+            this.Padding = new System.Windows.Forms.Padding(0, 2, 0, 2);
             this.Text = "Geography Game";
             this.mainTabControl.ResumeLayout(false);
             this.mainMenuTab.ResumeLayout(false);
