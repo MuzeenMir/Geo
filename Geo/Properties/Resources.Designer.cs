f2229b4fe285a21f770a5518d9dabdc020fcc485
Meany <mirmuzeen34@gmail.com>
2025-04-09 16:55:12 -0500

    jk

--- Geo/Properties/Resources.Designer.cs
+++ Geo/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Geo.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap wrld_21 {
+            get {
+                object obj = ResourceManager.GetObject("wrld-21", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
